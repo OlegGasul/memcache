e3d510cab6caf5f97c117a73bc4602cd9ed841d4
OlegGasiul <oleg.gasiul@informa.com>
2018-02-05 03:23:02 +0100

    Added Ratpack files

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -1,38 +1,20 @@
-import ratpack.example.groovywebconsole.ReloadingThing
-import ratpack.example.groovywebconsole.ScriptExecutionModule
-import ratpack.example.groovywebconsole.ScriptExecutor
-import ratpack.form.Form
-import ratpack.groovy.template.TextTemplateModule
+import com.memcache2.KeyValueHandlers
+import com.memcache2.MemcacheModule
+import com.memcache2.SearchHandler
 
-import static ratpack.groovy.Groovy.groovyTemplate
 import static ratpack.groovy.Groovy.ratpack
 
 ratpack {
 
 	bindings {
-		module ScriptExecutionModule
-		module TextTemplateModule, { TextTemplateModule.Config config -> config.staticallyCompile = true }
+		module MemcacheModule
 	}
 
 	handlers {
-		get {
-			render groovyTemplate("skin.html", title: "Groovy Web Console")
+		prefix("data/:key") {
+			insert KeyValueHandlers
 		}
-
-		post("execute") { ScriptExecutor scriptExecutor ->
-			parse(Form).then { Form form ->
-				String script = form.script
-				render scriptExecutor.execute(script)
-			}
-		}
-
-		get("reloadexample") {
-			render new ReloadingThing().toString()
-		}
-
-		fileSystem "public", { f -> f.files() }
+		post "search", new SearchHandler()
 	}
 
 }
-
-
